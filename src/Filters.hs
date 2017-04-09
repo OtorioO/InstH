b0ed047676b336f2f651fc56b25dc6fe6d5e3a4f
android <comp90194@mail.ru>
2017-04-09 16:31:16 +0300

    Новые фильтры

--- src/Filters.hs
+++ src/Filters.hs
@@ -34,4 +34,74 @@ maybefilters imgin imgout =
                      --do something
                      saveJpegFile 95 imgout x
                      putStrLn "OK")
+
+embfilters :: String -> String -> IO ()
+embfilters imgin imgout = 
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     emboss x
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+kelvin :: String -> String -> IO ()
+kelvin imgin imgout = 
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     colorize x  (255,153,0,50)
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+meanR :: String -> String -> IO ()
+meanR imgin imgout = 
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     meanRemoval x
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+negativfiltr :: String -> String -> IO ()
+negativfiltr imgin imgout = 
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     negative x
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+blackwhite :: String -> String -> IO ()
+blackwhite imgin imgout = 
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     grayscale x
+		     colorize x  (0,0,0,1)
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+bright :: String -> String -> Int -> IO ()
+bright imgin imgout colvo = --контрастность
+	 withImage  (case (map toLower $ takeExtension imgin) of
+                          ".jpg" -> loadJpegFile imgin  
+                          ".png" -> loadPngFile imgin 
+                          ".gif" -> loadGifFile imgin ) 
+		   (\x -> do
+                     brightness img colvo
+                     saveJpegFile 95 imgout x
+                     putStrLn "OK")
+
+
+
 	
